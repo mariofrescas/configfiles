552095ac984b410336de8ce8665fa66f4bf66ee3
mariofrescas <52926224+mariofrescas@users.noreply.github.com>
2021-10-30 00:40:30 -0500

    Add zsh plugin install

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,4 +7,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 ln -s $CURRENT_DIR/.zshrc $CONFIG_DIR/.zshrc
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 chsh -s $(which zsh)
