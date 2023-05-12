c6630b939a18c0e559f6b573be3e8e32ac6ad4a0
mariofrescas <mario.frescas.marquez@gmail.com>
2023-05-12 13:54:52 -0600

    Disables fbd bclose so sneak can use it

--- nvim/lua/lua/plugins/ranger.lua
+++ nvim/lua/lua/plugins/ranger.lua
@@ -4,5 +4,8 @@ vim.g.ranger_map_keys = 0
 -- Open ranger when vim open a directory
 vim.g.ranger_replace_netrw = 1
 
+-- Disable <leader>bd, so sneak.vim can use it
+vim.g.bclose_no_plugin_maps = true
+
 vim.api.nvim_set_keymap('n', '<leader>e', ':RangerCurrentFile<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>o', ':RangerWorkingDirectory<cr>', { noremap = true })
